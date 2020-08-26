52a055be3a1f587ee029432caacd8dd3d5623aeb
Givaildo de Andrade <gil.nmy@gmail.com>
2020-08-26 16:33:52 -0300

    Declararando Variaveis

--- 02tiposBasicosDados/main.c
+++ 02tiposBasicosDados/main.c
@@ -3,9 +3,10 @@
 int main(void){
     char nome_da_variavel;
     char segunda_variavel;
-    char x;
-    char y;
-    char a,b,c;
-    printf("Conteudo de a: %c\nConteudo de b: %c.",a,b);
+    char x = 'x';
+    char y = 'a';
+    float pi = 3.14;
+    printf("Conteudo de a: %c\nConteudo de b: %c.",x,y);
+    printf("\n%f",pi);
     return 0;
 }
