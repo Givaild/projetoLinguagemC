6adb2631771922688348fcbf224afd8befa9d503
Givaildo de Andrade <gil.nmy@gmail.com>
2020-08-26 16:27:35 -0300

    incluir pastas

---
+++ 02tiposBasicosDados/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void){
+    char nome_da_variavel;
+    char segunda_variavel;
+    char x;
+    char y;
+    char a,b,c;
+    printf("Conteudo de a: %c\nConteudo de b: %c.",a,b);
+    return 0;
+}
