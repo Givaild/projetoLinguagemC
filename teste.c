d869e7da876208a2c35ec2824c07277bee224bb3
Givaildo de Andrade <gil.nmy@gmail.com>
2020-08-26 14:16:20 -0300

    variave, e entrada de dados

--- teste.c
+++ teste.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 int main (void) {
+int n;// variavel do tipo inteiro
+scanf("%d",&n);/* %d é um padrão que representa um tipo de dado inteiro,depois das aspas eu informo a localização da variável onde a informação que eu vou ler do teclado será armazenada faço isso utilizando & e o nome da variavel no caso &n*/
+
+
 
     printf("\nOlá mundo!\n");
-   
+   printf("\nN = %d\n",n);
     return 0;
 }
