270978f513abf951ff889e5275758e1e23f3c8e7
Givaildo de Andrade <gil.nmy@gmail.com>
2020-08-27 08:51:16 -0300

    Atribuicoes

---
+++ 03Operadores/exOperadores.c
@@ -0,0 +1,11 @@
+# include <stdio.h>
+int main (void){
+    int a,b,c;
+    a = 2;
+    b = 3;
+    printf("a = %d e b = %d\n",a,b);
+     c = a + b;
+    printf("%d + %d = %d\n",a,b,c);
+    return 0;
+
+}
