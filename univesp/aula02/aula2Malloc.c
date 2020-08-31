6a160215625bf7871df67689d717ea6e34c348b3
Givaildo de Andrade <gil.nmy@gmail.com>
2020-08-31 08:21:04 -0300

    inclusão de comentarios

--- univesp/aula02/aula2Malloc.c
+++ univesp/aula02/aula2Malloc.c
@@ -7,8 +7,8 @@ typedef struct{
 }pesoAltura;
 
 int main(){
-    pesoAltura* pessoa1 = (pesoAltura*)malloc(sizeof(pesoAltura));
-    pessoa1->peso = 80;
+    pesoAltura* pessoa1 = (pesoAltura*)malloc(sizeof(pesoAltura));// funcao sizeof(pesoAltura é o tamanho em bytes) o ponteiro retornado pelo malloc é do tipo pesoAltura
+    pessoa1->peso = 80;//para acessar um ponteiro uso seta ->
     pessoa1->altura = 185;
     printf("Peso %d, Altura %d. ",pessoa1->peso,pessoa1->altura);
     if (pessoa1->altura > alturaMaxima) printf("Altura acima da Máxima\n");
